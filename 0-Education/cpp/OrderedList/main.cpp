cec8ea776eda8b5346ad4d23355130aca7753790
jaeho <human@handong.ac.kr>
2024-04-05 17:28:44 +0900

    cpp/orderedList.cpp

--- 0-Education/cpp/OrderedList/main.cpp
+++ 0-Education/cpp/OrderedList/main.cpp
@@ -7,46 +7,94 @@ void simpleTest1(OrderedList* o) {
 	add(o, 3);
 	add(o, 80);
 	add(o, 50);
+
 	for (int i = 0; i < o->m_size; ++i) {
-		std::cout << getValue(o, i) << ",	";
+		std::cout << getValue(o, i) << ", ";
 	}
 	std::cout << std::endl;
 }
+
 void simpleTest2(OrderedList* o) {
 	int vals[] = { 10, 20, 35, 35, 10 };
 	add(o, vals, sizeof(vals) / sizeof(int));
+
 	for (int i = 0; i < o->m_size; ++i) {
-		std::cout << getValue(o, i) << ",	";
+		std::cout << getValue(o, i) << ", ";
 	}
 	std::cout << std::endl;
 }
+
 void simpleTest3(OrderedList* o) {
 	remove(o, 3);
+
 	for (int i = 0; i < o->m_size; ++i) {
-		std::cout << getValue(o, i) << ",	";
+		std::cout << getValue(o, i) << ", ";
 	}
 	std::cout << std::endl;
 }
+
 void simpleTest4(OrderedList* o) {
 	std::cout << std::boolalpha << contains(o, 20) << std::endl;
 	std::cout << std::boolalpha << contains(o, 40) << std::endl;
 }
+
 void simpleTest5(OrderedList* o) {
 	std::cout << size(o) << std::endl;
 }
 
+void simpleTest6(OrderedList* o) {
+	remove(o, 0);
+
+	for (int i = 0; i < o->m_size + 1; ++i) {
+		std::cout << getValue(o, i) << ", ";
+	}
+	std::cout << std::endl;
+}
+
+void simpleTest7(OrderedList* o) {
+	int vals[] = { 90, 0, 2, 7, -1 };
+	add(o, vals, sizeof(vals) / sizeof(int));
+
+	for (int i = -1; i < o->m_size + 1; ++i) {
+		std::cout << getValue(o, i) << ", ";
+	}
+	std::cout << std::endl;
+}
+
+void simpleTest8(OrderedList* o) {
+	remove(o, 13);
+	remove(o, -1);
+	remove(o, 12);
+
+	for (int i = 0; i < o->m_size; ++i) {
+		std::cout << getValue(o, i) << ", ";
+	}
+	std::cout << std::endl;
+}
+
 int main()
 {
-	OrderedList orderedList;
-	std::cout << "<<Simple	Test	1>>" << std::endl;
-	simpleTest1(&orderedList);
-	std::cout << "<<Simple	Test	2>>" << std::endl;
-	simpleTest2(&orderedList);
-	std::cout << "<<Simple	Test	3>>" << std::endl;
-	simpleTest3(&orderedList);
-	std::cout << "<<Simple	Test	4>>" << std::endl;
-	simpleTest4(&orderedList);
-	std::cout << "<<Simple	Test	5>>" << std::endl;
-	simpleTest5(&orderedList);
+	OrderedList* orderedList = new OrderedList;
+	orderedList->head = nullptr;
+	orderedList->m_size = 0;
+
+	std::cout << "<<Simple Test 1>>" << std::endl;
+	simpleTest1(orderedList);
+	std::cout << "<<Simple Test 2>>" << std::endl;
+	simpleTest2(orderedList);
+	std::cout << "<<Simple Test 3>>" << std::endl;
+	simpleTest3(orderedList);
+	std::cout << "<<Simple Test 4>>" << std::endl;
+	simpleTest4(orderedList);
+	std::cout << "<<Simple Test 5>>" << std::endl;
+	simpleTest5(orderedList);
+
+	std::cout << "<<Simple Test 6>>" << std::endl;
+	simpleTest6(orderedList);
+	std::cout << "<<Simple Test 7>>" << std::endl;
+	simpleTest7(orderedList);
+	std::cout << "<<Simple Test 8>>" << std::endl;
+	simpleTest8(orderedList);
+
 	return 0;
 }
--- 0-Education/cpp/OrderedList/orderedList.cpp
+++ 0-Education/cpp/OrderedList/orderedList.cpp
@@ -1,25 +1,82 @@
 #include "orderedList.h"
+#include <limits>
+#include <iostream>
 
 void add(OrderedList* ordered, int v) {
+	Node* node = new Node;
+	node->element = v;
+	node->next = nullptr;
 
+	Node* crnt = ordered->head;
+	if (crnt == nullptr) {
+		ordered->head = node;
+		ordered->m_size++;
+	}
+	else if (v < crnt->element) {
+		node->next = crnt;
+		ordered->head = node;
+		ordered->m_size++;
+	}
+	else {
+		for (; crnt->next != nullptr; crnt = crnt->next) {
+			if (v < crnt->next->element) {
+				node->next = crnt->next;
+				crnt->next = node;
+				ordered->m_size++;
+				return;
+			}
+		}
+		crnt->next = node;
+		ordered->m_size++;
+	}
 }
 
 void add(OrderedList* ordered, const int* arr, int size) {
-
+	for (int i = 0; i < size; i++) {
+		add(ordered, arr[i]);
+	}
 }
 
 void remove(OrderedList* ordered, int index) {
+	if (index < 0 || ordered->m_size <= index) {
+		std::cout << "remove(): Out-of-index" << std::endl;
+		return;
+	}
 
+	Node* crnt = ordered->head, * prev = nullptr;
+	if (index == 0) {
+		ordered->head = crnt->next;
+	}
+	else {
+		for (int i = 0; i < index; i++) {
+			prev = crnt;
+			crnt = crnt->next;
+		}
+		prev->next = crnt->next;
+	}
+	ordered->m_size--;
 }
 
 int size(OrderedList* ordered) {
-
+	return ordered->m_size;
 }
 
 bool contains(OrderedList* ordered, int v) {
-
+	for (Node* node = ordered->head; node != nullptr; node = node->next)
+		if (node->element == v)
+			return true;
+	return false;
 }
 
 int getValue(OrderedList* ordered, int idx) {
+	if (idx < 0)
+		return std::numeric_limits<int>::min();
 
+	Node* result = ordered->head;
+	for (int i = 0; i < idx; i++) {
+		result = result->next;
+		if (result == nullptr)
+			return std::numeric_limits<int>::min();
+	}
+	return result->element;
 }
