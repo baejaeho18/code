f97aa4158af7783a47a8732a6f7616e43faf234f
jaeho <human@handong.ac.kr>
2024-06-07 15:48:17 +0900

    toy/MultiHeadList iterator

--- 2-toys/MultiHeadList/MultiHeadList.h
+++ 2-toys/MultiHeadList/MultiHeadList.h
@@ -2,6 +2,10 @@
 #ifndef MULTIHEADLIST_H
 #define MULTIHEADLIST_H
 
+#include <vector>
+
+
+template <typename T>
 struct Node {
     T data;
     Node* prev;
@@ -9,12 +13,35 @@ struct Node {
     Node(const T& data) : data(data), prev(nullptr), next(nullptr) {}
 };
 
+template <typename T>
 class MultiHeadList {
 private:
     vector<Node<T>*> headList;
 public :
-    class Iterator {};
-    class ReverseIterator{};
+    class Iterator {
+    public:
+        Node<T>* curr;
+        Iterator(Node<T>* node) : curr(node) {}
+        T& operator*();
+        Iterator& operator++();
+        Iterator operator++(int);
+        Iterator& operator--();
+        Iterator operator--(int);
+        bool operator!=(const Iterator& other) const;
+        bool operator==(const Iterator& other) const;
+    };
+    class ReverseIterator{
+    public:
+        Node<T>* curr;
+        ReverseIterator(Node<T>* node) : curr(node) {}
+        T& operator*();
+        ReverseIterator& operator++();
+        ReverseIterator operator++(int);
+        ReverseIterator& operator--();
+        ReverseIterator operator--(int);
+        bool operator!=(const ReverseIterator& other) const;
+        bool operator==(const ReverseIterator& other) const;
+    };
 
 public:
     int headSize() {}
