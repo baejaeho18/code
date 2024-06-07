0db103391acea9ae38dd699f489806f8a1ac48d4
jaeho <human@handong.ac.kr>
2024-06-07 15:51:21 +0900

    toy/MultiHeadList syllogism

--- 2-toys/MultiHeadList/Syllogism.cpp
+++ 2-toys/MultiHeadList/Syllogism.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 #include "MultiHeadList.h"
 
 using namespace std;
@@ -6,9 +7,36 @@ using namespace std;
 class Syllogism {
 private :
 	MultiHeadList<pair<string,string>>syl;
+
 public :
-	void put(const pair<string, string>& argument) {}
-	void qna(const string& q) {}
+	void put(const pair<string, string>& argument) {
+		for (int i = 0; i < syl.headSize(); ++i) {
+			auto it = syl.begin(i);
+			while (it != syl.end() && it.curr->data.second != argument.first) ++it;
+			if (it != syl.end()) {
+				syl.push_back(argument, i);
+				return;
+			}
+		}
+		syl.push_back(argument);
+	}
+	void qna(const string& q) {
+		for (int i = 0; i < syl.headSize(); ++i) {
+			auto it = syl.begin(i);
+			while (it != syl.end() && it.curr->data.first != q) ++it;
+			if (it != syl.end()) {
+				std::string result = it.curr->data.second;
+				++it;
+				while (it != syl.end()) {
+					result = it.curr->data.second;
+					++it;
+				}
+				std::cout << "If " << q << ", then " << result << std::endl;
+				return;
+			}
+		}
+	}
+
 	void print() {
 		cout << "=============" << endl;
 		for (int i = 0; i != syl.headSize(); i++) {
