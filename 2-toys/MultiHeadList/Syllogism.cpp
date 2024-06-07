a1ef431ed7ce62dbfdd0c3f71b0299fe96680ef6
jaeho <human@handong.ac.kr>
2024-06-07 18:30:06 +0900

    err: toy/MultiHeadList put_to_front

--- 2-toys/MultiHeadList/Syllogism.cpp
+++ 2-toys/MultiHeadList/Syllogism.cpp
@@ -12,18 +12,27 @@ public :
 	void put(const pair<string, string>& argument) {
 		for (int i = 0; i < syl.headSize(); ++i) {
 			auto it = syl.begin(i);
-			while (it != syl.end() && it.curr->data.second != argument.first) ++it;
-			if (it != syl.end()) {
-				syl.push_back(argument, i);
-				return;
+			while (it != syl.end()) {
+				if (it.curr->data.second == argument.first) {
+					syl.push_back(argument, i);
+					return;
+				}
+				if (it.curr->data.first == argument.second) {
+					syl.insert(it, argument);
+					return;
+				}
+				++it;
 			}
 		}
 		syl.push_back(argument);
 	}
+
 	void qna(const string& q) {
 		for (int i = 0; i < syl.headSize(); ++i) {
 			auto it = syl.begin(i);
-			while (it != syl.end() && it.curr->data.first != q) ++it;
+			while (it != syl.end() && it.curr->data.first != q) 
+				++it;
+			
 			if (it != syl.end()) {
 				std::string result = it.curr->data.second;
 				++it;
@@ -41,7 +50,7 @@ public :
 		cout << "=============" << endl;
 		for (int i = 0; i != syl.headSize(); i++) {
 			cout << i << " : ";
-			for (auto it = syl.begin(i); it != syl.end(); it++) {
+			for (auto it = syl.begin(i); it != syl.end(); ++it) {
 				if (it == syl.begin(i)) {
 					cout << (*it).first << "->" << (*it).second;
 				}
