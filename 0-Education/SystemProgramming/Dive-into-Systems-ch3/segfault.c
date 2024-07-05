de6d24f0ea44cd97738e6c8e9b1f3195e015c61f
jaeho <human@handong.ac.kr>
2024-07-05 23:18:04 +0900

    [SysProg] fix: badprog.c segfault.c

--- 0-Education/SystemProgramming/Dive-into-Systems-ch3/segfault.c
+++ 0-Education/SystemProgramming/Dive-into-Systems-ch3/segfault.c
@@ -1,38 +1,28 @@
-/*
- * Copyright (c) 2020, Dive into Systems, LLC (https://diveintosystems.org/)
- * Program with a segfault
- */
+/* segfault.c  finds the largest element in the array */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-int initfunc(int *array, int len) {
-    int i;
-    for (i=1; i <= len; i++)
+int initfunc(int* array, int len) {
+    for (int i = 1; i <= len; i++)
         array[i] = i;
     return 0;
 }
-
-int func(int *array1, int len, int max) {
-    int i;
+int func(int* array1, int len, int max) {
     max = array1[0];
-    for (i=1; i <= len; i++) {
-        if (max < array1[i]) {
+    for (int i = 1; i < len; i++)
+        if (max < array1[i])
             max = array1[i];
-        }
-    }
     return 0;
 }
-
-int main(int argc, char *argv[]) {
-    int *arr = NULL;
+int main(int argc, char* argv[]) {
+    int* arr = malloc(100 * sizeof(int));
     int max = 6;
-
-    if (initfunc(arr, 100) != 0 ) {
+    if (initfunc(arr, 100) != 0) {
         printf("init error\n");
         exit(1);
     }
-    if ( func(arr, 100, max) != 0 ) {
+    if (func(arr, 100, max) != 0) {
         printf("func error\n");
         exit(1);
     }
