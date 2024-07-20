9842023c2a9b5995d5a88939b56286018b990b83
jaeho <human@handong.ac.kr>
2024-07-20 18:58:06 +0900

    sysprog: ch17.5-makefile CMake added

---
+++ 0-Education/SystemProgramming/Dive-into-Systems-ch17.5/build/CMakeFiles/myprog.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/bjh17/Projects/code/0-Education/SystemProgramming/Dive-into-Systems-ch17.5/bar.c" "CMakeFiles/myprog.dir/bar.c.o" "gcc" "CMakeFiles/myprog.dir/bar.c.o.d"
+  "/mnt/c/Users/bjh17/Projects/code/0-Education/SystemProgramming/Dive-into-Systems-ch17.5/foo.c" "CMakeFiles/myprog.dir/foo.c.o" "gcc" "CMakeFiles/myprog.dir/foo.c.o.d"
+  "/mnt/c/Users/bjh17/Projects/code/0-Education/SystemProgramming/Dive-into-Systems-ch17.5/main.c" "CMakeFiles/myprog.dir/main.c.o" "gcc" "CMakeFiles/myprog.dir/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 0-Education/SystemProgramming/Dive-into-Systems-ch17.5/build/CMakeFiles/myprog.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myprog.dir/bar.c.o"
+  "CMakeFiles/myprog.dir/bar.c.o.d"
+  "CMakeFiles/myprog.dir/foo.c.o"
+  "CMakeFiles/myprog.dir/foo.c.o.d"
+  "CMakeFiles/myprog.dir/main.c.o"
+  "CMakeFiles/myprog.dir/main.c.o.d"
+  "myprog"
+  "myprog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/myprog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
