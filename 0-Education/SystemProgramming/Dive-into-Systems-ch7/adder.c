9c71d54cfc1fe3d329a7ed5e05944c600cb1014f
jaeho <human@handong.ac.kr>
2024-08-02 11:40:29 +0900

    sysporg: ch7-assembly adder.c

---
+++ 0-Education/SystemProgramming/Dive-into-Systems-ch7/adder.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int adder() {
+    int a;
+    return a+2;
+}
+
+int assign() {
+    int y = 40;
+    return y;
+}
+
+int main() {
+    int x;
+    assign();
+    x = adder();
+    printf("x is: %d", x);
+
+    return 0;
+}
