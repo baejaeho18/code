c8c70c93764824d10b2b983f9e1aede7324716a1
jaeho <human@handong.ac.kr>
2024-08-05 17:21:12 +0900

    sysprog: adanced_C bitwise.c

--- 0-Education/SystemProgramming/Dive-into-Systems-ch2and4/bitwise.c
+++ 0-Education/SystemProgramming/Dive-into-Systems-ch2and4/bitwise.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 
+bdisp(c)
+int c;
+{
+    int i, wc, d;
+    d = c;
+    for (i=15; i>=0; i--) {
+        wc = (c >> i) & 0x01;
+        printf("%1d", wc);
+        c = d;
+    }
+    printf("\n");
+}
+
 int main() {
-    unsigned char a = 13;
-    unsigned char b = 22;
+    
+    int x = 123, y;
 
-    unsigned char sl_a = a << 3;  // Shift left by 3 bits
-    unsigned char sr_a = a >> 3;  // Shift right by 3 bits
-    unsigned char neg_a = ~a;     // Bitwise NOT
-    unsigned char a_and_b = a & b;  // Bitwise AND
-    unsigned char a_or_b = a | b;   // Bitwise OR
-    unsigned char a_xor_b = a ^ b;  // Bitwise XOR
+    printf("%7s%15s\t%s\n", "decimal", "hexadecimal", "bit pattern");
+    printf("%7d%15x\t", x, x); bdisp(x);
 
-    printf("a = %d \t: 0x%02X\n", a, a);
-    printf("b = %d \t: 0x%02X\n", b, b);
-    printf("sl_a = %d \t: 0x%02X\n", sl_a, sl_a);
-    printf("sr_a = %d \t: 0x%02X\n", sr_a, sr_a);
-    printf("neg_a = %d \t: 0x%02X\n", neg_a, neg_a);
-    printf("a_and_b = %d \t: 0x%02X\n", a_and_b, a_and_b);
-    printf("a_or_b = %d \t: 0x%02X\n", a_or_b, a_or_b);
-    printf("a_xor_b = %d \t: 0x%02X\n", a_xor_b, a_xor_b);
+    y = ~x;
+    printf("%7d%15x\t", y, y); bdisp(y);
+
+    y = x | 128;
+    printf("%7d%15x\t", y, y); bdisp(y);
 
     return 0;
 }
