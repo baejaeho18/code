37512970f2e21f1f78f46c5be1d0ba803743d7ef
jaeho <human@handong.ac.kr>
2024-08-24 03:32:16 +0900

    sysprog: ptr_arith.c

--- 0-Education/SystemProgramming/Dive-into-Systems-ch2and4/ptr_arith.c
+++ 0-Education/SystemProgramming/Dive-into-Systems-ch2and4/ptr_arith.c
@@ -2,12 +2,27 @@
 
 int main(void)
 {
-	double a[2], * p, * q;
-	p = &a[0];
-	q = p + 1;
+#define N 5
+	int a[N], * p, * q, * r, i;
+	p = a;      // Same as p = &a[0];
+	q = a + 3;    // Same as q = &a[3]; Adding an integer to a pointer
+	r = q - 2;    // Same as p = &a[1]; Subtracting an integer from a pointer
+	i = q - p;    // Same as i = 3; Subtracting pointers results in the difference in the number of elements between two pointers
+	// Comparison pointer depends on the relative position of two pointers
+	if (p > q) printf(¡°pointer p has a small index value¡±); // (p > q) is 0
+	if (p < q) printf(¡°pointer p has a large index value¡±); // (p < q) is 1
+	if (p == q) printf(¡°equal index value¡±);         		// (p == q) is 0
 
-	printf("%d\n", q - p);
-	printf("%d\n", (int)q - (int));
+
+
+
+	double b[2], * s, * t;
+	s = &b[0];
+	t = s + 1;
+
+	printf("%d\n", t - s);
+	printf("%d\n", (int)t - (int)s);
 	printf("%d\n", sizeof(double));
+
 	return 0;
 }
